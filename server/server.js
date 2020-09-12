47f8f85fdef4b879c34f4b207b021ab5fccad02f
ARCYNTRAR <111.contrerasramirez@gmail.com>
2020-09-11 23:11:38 -0500

    RestServer Usuario

--- server/server.js
+++ server/server.js
@@ -1,53 +1,35 @@
-require('./config/config');
+require("./config/config");
 
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
- 
+
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require("./routes/usuario"));
 
-app.get('/usuario', function (req, res) {
-  res.json('get usuario');
-});
-
-app.post('/usuario', function (req, res) {
-
-  let body = req.body;
-
-  if( body.nombre === undefined ){
-
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    });
-
-  }else{
-    res.json({
-      persona: body
-    });
-  }
-
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-  let id = req.params.id;
-
-  res.json({
-    id
-  });
-});
-
-app.delete('/usuario', function (req, res) {
-  res.json('delete usuario');
-});
- 
 app.listen(process.env.PORT, () => {
-    console.log(`Escuchando puerto ${process.env.PORT}`);
+  console.log(`Escuchando puerto ${process.env.PORT}`);
 });
+
+mongoose.connect(
+  process.env.URLDB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  (error, res) => {
+    if (error) throw error;
+
+    console.log("On tas Base que no te veo buguiarte!!!");
+  }
+);
