046d8d0b5302b495d342f1c3d05c29ee8f7abd09
ARCYNTRAR <111.contrerasramirez@gmail.com>
2020-09-11 23:28:16 -0500

    Cambio en password

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = "mongodb://localhost:27017/cafe";
 } else{
-    urlDB = 'mongodb+srv://ARCYNTRAR:PoolConexion7|@cursonode.szhjo.mongodb.net/cafe'
+    urlDB = 'mongodb+srv://ARCYNTRAR:PoolConexion7@cursonode.szhjo.mongodb.net/cafe'
 }
 
 process.env.URLDB = urlDB;
