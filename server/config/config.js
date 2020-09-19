0c082bdbc37ae42ee9649e8852dad98e6e2a2d31
ARCYNTRAR <111.contrerasramirez@gmail.com>
2020-09-18 21:26:28 -0500

    Arreglado el problema de duración del token

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // 60 segundos, 60 minutos, 24 horas, 30 dias
 
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = (Date.now() / 1000) + 60 * 30;
 
 //  ================================
 //          Seed de Auth
